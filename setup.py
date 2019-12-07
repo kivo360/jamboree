724108ffda01ead0bd93b7a9bb924c523b1ac05c
kivo360 <kivo360@gmail.com>
2019-12-07 18:09:23 -0500

    Fixed the bulk add command

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class UploadCommand(Command):
 
 setup(
     name="jamboree",
-    version="0.1.8",
+    version="0.1.9",
     author="Kevin Hill",
     author_email="kah.kevin.hill@gmail.com",
     description="A fast abstracted event driven library for mongo/redis. Meant for speed",
